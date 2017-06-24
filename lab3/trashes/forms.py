1938e9a5012957cfcc5aaf0ccedef7a259979e4c
vereline <vstanko1998@gmail.com>
2017-06-25 02:33:31 +0300

    improved models of trash and task

--- lab3/trashes/forms.py
+++ lab3/trashes/forms.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from .models import Trash
+from .models import Trash, TaskToDo
 
 
 class TrashForm(forms.ModelForm):
 
     class Meta:
         model = Trash
-        fields = ('name', 'path', 'info_path', )
+        fields = ('name', 'path', 'info_path', 'maximum_size', 'maximum_time', 'policy_size',
+                  'policy_time', )
+
+
+class TaskForm(forms.ModelForm):
+
+    class Meta:
+        model = TaskToDo
+        fields = ('file_path', 'file_task', 'info_path', 'force', 'dry_run', 'silent',
+                  'task_is_done', 'maximum_time', 'maximum_size', 'trash', 'regular', )
--- lab3/trashes/models.py
+++ lab3/trashes/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import os
 # Create your models here.
 
 
@@ -10,17 +10,39 @@ class Trash(models.Model):
     name = models.CharField(max_length=256)
     path = models.CharField(max_length=256)
     info_path = models.CharField(max_length=256)
+    maximum_size = models.IntegerField(default=1000)
+    maximum_time = models.IntegerField(default=7)
+    policy_time = models.BooleanField(default=False)
+    policy_size = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'trashes'
 
-    # def __str__(self):              # __unicode__ on Python 2
-    #     return self.path
+    def __str__(self):              # __unicode__ on Python 2
+        return os.path.abspath(self.path)
 
 
 class TaskToDo(models.Model):
-    file_task = models.CharField(max_length=256)
-    file_path = models.CharField(max_length=256)
+    # file_task = models.CharField(max_length=256)
+    file_path = models.CharField(max_length=256, default=' ')
+    trash = models.ForeignKey(Trash, default=None)
+    force = models.BooleanField(default=False)
+    dry_run = models.BooleanField(default=False)
+    silent = models.BooleanField(default=False)
+
+    tasks = (
+        ('delete by regex', 'delete by regex'),
+        ('delete by filename', 'delete by filename'),
+        ('recover all trash', 'recover all trash'),
+        ('clear all trash', 'clear all trash')
+    )
+
+    file_task = models.CharField(max_length=256, choices=tasks, default=None)
+    regular = models.CharField(max_length=256, default='')
+    info_path = models.CharField(max_length=256)
+    maximum_size = models.IntegerField(default=1000)
+    maximum_time = models.IntegerField(default=7)
+    task_is_done = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'task'
