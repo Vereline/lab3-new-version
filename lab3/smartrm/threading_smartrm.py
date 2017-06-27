a7c3c0fa6eabc7d520bbb8020fc15cd43859a281
vereline <vstanko1998@gmail.com>
2017-06-27 17:36:21 +0300

    created template versions of parallel requests

---
+++ lab3/smartrm/threading_smartrm.py
@@ -0,0 +1,13 @@
+#! usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import threading
+
+def parallel_remove():
+    pass
+
+def parallel_restore():
+    pass
+
+def parallel_regex_remove():
+    pass
