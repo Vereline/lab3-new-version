9333a388474021d2918fcf4e7b423eb41c5263c7
vereline <vstanko1998@gmail.com>
2017-08-09 23:51:04 +0300

    some threading details

--- lab3/smartrm/threading_smartrm.py
+++ lab3/smartrm/threading_smartrm.py
@@ -3,6 +3,37 @@
 
 import threading
 
+class WorkingThread(threading.Thread):
+    def __init__(self, filename_list, filename_list_lock):
+        super(WorkingThread, self).__init__()
+        self.filename_list = filename_list
+        self.filename_list_lock = filename_list_lock
+
+    def run(self):
+        while (True):
+            next_filename = self.grab_next_filename()
+            if next_filename is None:
+                break
+            self.retrieve_filename(next_filename)
+
+    def grab_next_filename(self):
+        self.filename_list_lock.acquire(1)
+        if len(self.filename_list) < 1:
+            next_filename = None
+        else:
+            next_filename = self.filename_list[0]
+            del self.filename_list[0]
+        self.filename_list_lock.release()
+
+        return next_filename
+
+    def retrieve_filename(self, next_filename):
+        # text = open(nexturl).read()
+        # print text
+        # print '################### %s #######################' % nexturl
+        pass
+
+
 def parallel_remove():
     pass
 
@@ -11,3 +42,15 @@ def parallel_restore():
 
 def parallel_regex_remove():
     pass
+
+def parallel_searching_directories():
+    pass
+
+def parallel_regex_restore():
+    pass
+
+# def parallel_regex_remove():
+#     pass
+#
+# def parallel_regex_remove():
+#     pass
